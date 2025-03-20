82f2409b774da176f2cdb058ffb6a272b69519a8
Sam MAI <sam008.gamefire@gmail.com>
2025-03-20 16:35:16 +0700

    Update services model logic

--- src/routes/v1/boardRoutes.js
+++ src/routes/v1/boardRoutes.js
@@ -18,4 +18,6 @@ Router.route('/')
   })
   .post(boardValidation.createNew, boardController.createNew)
 
+Router.route('/:id').get(boardController.getDetails).put()
+
 export const boardRoutes = Router
